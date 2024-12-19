3c591415a9e50f18629dfd3538a02a8b232eb7ea
nguyentrungtinh <trungtinhn300@gmail.com>
2024-12-19 14:46:52 +0700

    Add file SwaggerConfig

---
+++ src/main/java/com/TrungTinhBackend/codearena_backend/Config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.TrungTinhBackend.codearena_backend.Config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicAPI() {
+        return GroupedOpenApi.builder()
+                .group("CodeArena-API")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
