1e952b0b773b3dc2f9ed995bef56be60fe9cf27a
nguyentrungtinh <trungtinhn300@gmail.com>
2025-01-22 14:18:50 +0700

    SCRUM-56 create API UPDATE quiz

--- src/main/java/com/TrungTinhBackend/codearena_backend/Controller/QuizController.java
+++ src/main/java/com/TrungTinhBackend/codearena_backend/Controller/QuizController.java
@@ -7,10 +7,7 @@ import com.TrungTinhBackend.codearena_backend.Service.Quiz.QuizService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -24,5 +21,11 @@ public class QuizController {
     public ResponseEntity<APIResponse> addQuiz(@Valid @RequestPart(value = "quiz") APIRequestQuiz apiRequestQuiz,
                                                  @RequestPart(value = "img") MultipartFile img) throws Exception {
         return ResponseEntity.ok(quizService.addQuiz(apiRequestQuiz, img));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<APIResponse> updateQuiz(@PathVariable Long id, @Valid @RequestPart(value = "quiz") APIRequestQuiz apiRequestQuiz,
+                                               @RequestPart(value = "img") MultipartFile img) throws Exception {
+        return ResponseEntity.ok(quizService.updateQuiz(id, apiRequestQuiz, img));
     }
 }
--- src/main/java/com/TrungTinhBackend/codearena_backend/Service/Quiz/QuizServiceImpl.java
+++ src/main/java/com/TrungTinhBackend/codearena_backend/Service/Quiz/QuizServiceImpl.java
@@ -2,8 +2,10 @@ package com.TrungTinhBackend.codearena_backend.Service.Quiz;
 
 import com.TrungTinhBackend.codearena_backend.Entity.Course;
 import com.TrungTinhBackend.codearena_backend.Entity.Lesson;
+import com.TrungTinhBackend.codearena_backend.Entity.Question;
 import com.TrungTinhBackend.codearena_backend.Entity.Quiz;
 import com.TrungTinhBackend.codearena_backend.Repository.LessonRepository;
+import com.TrungTinhBackend.codearena_backend.Repository.QuestionRepository;
 import com.TrungTinhBackend.codearena_backend.Repository.QuizRepository;
 import com.TrungTinhBackend.codearena_backend.Request.APIRequestQuiz;
 import com.TrungTinhBackend.codearena_backend.Response.APIResponse;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class QuizServiceImpl implements QuizService{
@@ -27,6 +30,8 @@ public class QuizServiceImpl implements QuizService{
     @Autowired
     private LessonRepository lessonRepository;
 
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @Override
     public APIResponse addQuiz(APIRequestQuiz apiRequestQuiz, MultipartFile img) throws Exception {
@@ -67,7 +72,54 @@ public class QuizServiceImpl implements QuizService{
 
     @Override
     public APIResponse updateQuiz(Long id, APIRequestQuiz apiRequestQuiz, MultipartFile img) throws Exception {
-        return null;
+        APIResponse apiResponse = new APIResponse();
+        try {
+            Lesson lesson = lessonRepository.findById(apiRequestQuiz.getLesson().getId()).orElseThrow(
+                    () -> new RuntimeException("Lesson not found !")
+            );
+
+            Quiz quiz = quizRepository.findById(id).orElseThrow(
+                    () -> new RuntimeException("Quiz not found !")
+            );
+            if(apiRequestQuiz.getQuizName() != null && !apiRequestQuiz.getQuizName().isEmpty()) {
+                quiz.setQuizName(apiRequestQuiz.getQuizName());
+            }
+            if(apiRequestQuiz.getDescription() != null && !apiRequestQuiz.getDescription().isEmpty()) {
+                quiz.setDescription(apiRequestQuiz.getDescription());
+            }
+
+            if(img != null) {
+                quiz.setImg(imgService.updateImg(quiz.getImg(),img));
+            }
+            if(lesson != null) {
+                quiz.setLesson(lesson);
+            }
+            if (apiRequestQuiz.getQuestions() != null && !apiRequestQuiz.getQuestions().isEmpty()) {
+                List<Question> questionList = questionRepository.findAllById(
+                        apiRequestQuiz.getQuestions().stream().map(Question::getId).toList()
+                );
+
+                if (questionList.size() != apiRequestQuiz.getQuestions().size()) {
+                    throw new RuntimeException("One or more questions not found!");
+                }
+
+                quiz.setQuestions(questionList);
+            }
+
+            quizRepository.save(quiz);
+
+            apiResponse.setStatusCode(200L);
+            apiResponse.setMessage("Update quiz success !");
+            apiResponse.setData(quiz);
+            apiResponse.setTimestamp(LocalDateTime.now());
+
+            return apiResponse;
+
+        } catch (BadCredentialsException e) {
+            throw new RuntimeException("Invalid credentials");
+        } catch (Exception e) {
+            throw new Exception("Message : "+e.getMessage(),e);
+        }
     }
 
     @Override
