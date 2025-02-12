cab78298d6881f675090237a5f91954a4b03b155
nguyentrungtinh <trungtinhn300@gmail.com>
2025-02-12 09:22:28 +0700

    Update file application.properties

--- src/main/java/com/TrungTinhBackend/codearena_backend/Service/Blog/BlogService.java
+++ src/main/java/com/TrungTinhBackend/codearena_backend/Service/Blog/BlogService.java
@@ -9,6 +9,5 @@ public interface BlogService {
     public APIResponse updateBlog(Long id, APIRequestBlog apiRequestBlog, MultipartFile img, MultipartFile video) throws Exception;
     public APIResponse deleteBlog(Long id) throws Exception;
     public APIResponse searchBlog(String keyword, int page, int size);
-    public APIResponse getBlogByPage(int page, int size);
-    public APIResponse getBlogById(Long id);
+
 }
