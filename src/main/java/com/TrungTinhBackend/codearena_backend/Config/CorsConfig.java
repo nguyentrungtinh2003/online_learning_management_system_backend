032c8bf9cffcbdcd6940e35f0f941a8d90426773
Nguyễn Trọng Hiếu <140324926+NguyenTrongHieu1403@users.noreply.github.com>
2025-03-16 13:56:25 +0700

    Update CorsConfig.java

--- src/main/java/com/TrungTinhBackend/codearena_backend/Config/CorsConfig.java
+++ src/main/java/com/TrungTinhBackend/codearena_backend/Config/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://codearena-backend-dev.onrender.com")
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
