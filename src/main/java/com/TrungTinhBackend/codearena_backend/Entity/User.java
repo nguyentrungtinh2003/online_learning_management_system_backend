365ab375f35f213714fbb84c32b9c228eecaaf23
nguyentrungtinh <trungtinhn300@gmail.com>
2025-03-26 10:51:02 +0700

    Update return role in entity user impl userDetails

--- src/main/java/com/TrungTinhBackend/codearena_backend/Entity/User.java
+++ src/main/java/com/TrungTinhBackend/codearena_backend/Entity/User.java
@@ -10,6 +10,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
@@ -104,7 +105,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_"+roleEnum.name()));
     }
 
     @Override
