64397b714fb5bc749c9100357de3bc91ecdd3126
nguyentrungtinh <trungtinhn300@gmail.com>
2025-04-16 17:45:05 +0700

    Update Json Reference to test

--- src/main/java/com/TrungTinhBackend/codearena_backend/Entity/Course.java
+++ src/main/java/com/TrungTinhBackend/codearena_backend/Entity/Course.java
@@ -1,6 +1,7 @@
 package com.TrungTinhBackend.codearena_backend.Entity;
 
 import com.TrungTinhBackend.codearena_backend.Enum.CourseEnum;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
@@ -43,6 +44,7 @@ public class Course {
 
     @ManyToOne()
     @JoinColumn(name = "user_id")
+    @JsonBackReference
     private User user;
 
     @OneToMany(mappedBy = "course")
