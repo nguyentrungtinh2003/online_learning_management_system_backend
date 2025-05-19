6bccf5202303939abf68570495ffd40cc6061f20
nguyentrungtinh <trungtinhn300@gmail.com>
2025-05-19 15:30:38 +0700

    Update file JwtAuthFilter

--- src/main/java/com/TrungTinhBackend/codearena_backend/Service/Jwt/JwtAuthFilter.java
+++ src/main/java/com/TrungTinhBackend/codearena_backend/Service/Jwt/JwtAuthFilter.java
@@ -95,7 +95,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         if (username != null) {
             boolean success = true;
             // Chỉ log nếu là endpoint quan trọng
-            if (!uri.startsWith("/api/ws") && !uri.contains("websocket")) {
+            if (!uri.startsWith("/ws") && !uri.contains("websocket")) {
                 loginLogService.save(new LoginLog(username, ipAddress, success, details));
             }
         }
